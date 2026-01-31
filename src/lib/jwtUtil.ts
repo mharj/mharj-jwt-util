1debf1adf79d2fe8cb870feb84cbb8ac4221159a
Marko Harjula <marko.harjula@gmail.com>
2026-01-31 09:20:06 +0200

    fixes to jwt module loading

--- src/lib/jwtUtil.ts
+++ src/lib/jwtUtil.ts
@@ -1,10 +1,10 @@
-import {verify, type VerifyErrors} from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 import type {TokenPayload} from '../interfaces/token';
 
-type JwtVerifyPromiseFunc<T = Record<string, unknown>> = (...params: Parameters<typeof verify>) => Promise<TokenPayload<T> | undefined>;
+type JwtVerifyPromiseFunc<T = Record<string, unknown>> = (...params: Parameters<typeof jwt.verify>) => Promise<TokenPayload<T> | undefined>;
 export const jwtVerifyPromise: JwtVerifyPromiseFunc = (token, secretOrPublicKey, options?) => {
 	return new Promise<TokenPayload | undefined>((resolve, reject) => {
-		verify(token, secretOrPublicKey, options, (err: VerifyErrors | null, decoded: object | string | undefined) => {
+		jwt.verify(token, secretOrPublicKey, options, (err: jwt.VerifyErrors | null, decoded: object | string | undefined) => {
 			if (err) {
 				reject(err);
 			} else {
